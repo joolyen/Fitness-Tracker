233b9a43a08e4a55852649be28474a68987a199e
joolyenIV <julianivey@gmail.com>
2020-12-07 19:48:19 -0600

    adding heroku postbuild to package.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-//const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to localhost or atlas URI defined in heroku
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://joolyen2377:<password>@clusterjohnson.mgfl3.mongodb.net/<dbname>?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
