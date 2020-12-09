531bc3c1c0a36cab3e20ccae5f3c7787752f75e9
joolyenIV <julianivey@gmail.com>
2020-12-08 23:58:08 -0600

    trying to get the db on atlas connected

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,9 +1,10 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 let workoutSeed = [
