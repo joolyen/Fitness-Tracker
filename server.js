6dc8a057477e1279c9d3f9a3c5e6ff9dceb8d3da
joolyenIV <julianivey@gmail.com>
2020-12-09 01:45:47 -0600

    adjustments to the server.js file

--- server.js
+++ server.js
@@ -2,27 +2,29 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const app = express();
 const PORT = process.env.PORT || 2377;
 
-const app = express();
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to localhost or atlas URI defined in heroku
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false,
   useUnifiedTopology: true
-});
+})
 
 // HTML Routes
 app.use(require("./routes/api-routes.js"))
 app.use(require("./routes/html-routes.js"))
 
 if (process.env.NODE_ENV === 'production'){
-  
+
 }
 
 app.listen(PORT, () => {
