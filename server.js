e31203757bb59566c0eb46db2309310c1b12c29f
joolyenIV <julianivey@gmail.com>
2020-12-09 00:08:14 -0600

    trying to get the db on atlas connected

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
@@ -19,8 +18,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // HTML Routes
-app.use(require("./routes/html-routes.js"))
 app.use(require("./routes/api-routes.js"))
+app.use(require("./routes/html-routes.js"))
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
